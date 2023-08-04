ecb639064c72940132a68ab46d2eeb575cbaef27
Maitri Vora <maitriv7@gmail.com>
2023-08-04 16:34:59 +0530

    Backend application with h2 db connectivity

---
+++ src/main/java/com/db/ApiApplication.java
@@ -0,0 +1,9 @@
+package com.db;
+
+import org.springframework.boot.SpringApplication;
+
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
